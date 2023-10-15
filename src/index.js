16c40a190101d83bbd843d3d2316e8a9eef3bad5
developer <1735721264@qq.com>
2023-10-15 18:20:58 +0800

    x

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom'
 
 import Layout from './View.Layout'
 
-import '../text-tokenizer/index'
-import '../text-generator/index'
-import '../text-calculator/index'
-
 import './index.css'
+
+import icon from '../static/icon.png'
+
+const link = document.createElement('link')
+link.rel = 'icon'
+link.href = icon
+document.head.append(link)
 
 ReactDOM.render(<Layout />, document.getElementById('root'))
